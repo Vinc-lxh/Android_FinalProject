3dd730e97253088a8af4192287179a9dd0b86b15
Deng Zou <zoud@zoud-stg4.rose-hulman.edu>
2022-01-13 18:15:52 -0500

    login

---
+++ app/src/main/java/edu/rosehulman/photovoicememo/ui/LogIn/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package edu.rosehulman.photovoicememo.ui.LogIn.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
