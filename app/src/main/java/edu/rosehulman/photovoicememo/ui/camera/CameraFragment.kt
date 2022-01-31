c4ef1300c6a5289232681186220fb2c82a2c2714
Deng Zou <zoud@zoud-stg4.rose-hulman.edu>
2022-01-31 01:31:48 -0500

    code with lin on my laptop
    Recorder, bugs remain

--- app/src/main/java/edu/rosehulman/photovoicememo/ui/camera/CameraFragment.kt
+++ app/src/main/java/edu/rosehulman/photovoicememo/ui/camera/CameraFragment.kt
@@ -2,6 +2,7 @@ package edu.rosehulman.photovoicememo.ui.camera
 
 import android.app.Activity
 import android.app.Activity.RESULT_OK
+import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
@@ -9,10 +10,6 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
@@ -23,19 +20,39 @@ import edu.rosehulman.photovoicememo.MainActivity
 import edu.rosehulman.photovoicememo.databinding.FragmentCameraBinding
 import java.util.jar.Manifest
 import android.content.ContentValues
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.net.Uri
+import android.os.SystemClock
+import android.widget.*
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.core.app.ActivityCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import edu.rosehulman.photovoicememo.R
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class CameraFragment : Fragment() {
 
     private lateinit var cameraViewModel: CameraViewModel
     private lateinit var binding: FragmentCameraBinding
-
+    private lateinit var mediaRecorder: MediaRecorder
+    private lateinit var navController: NavController
+    private var isRecording: Boolean = false
+    private var recordFile: String = ""
+    private lateinit var doneBtn: ImageButton
+    private lateinit var recordBtn: ImageButton
+    private lateinit var filenameText: TextView
+    private lateinit var timer: Chronometer
     // This property is only valid between onCreateView and
     // onDestroyView.
     lateinit var imageView:ImageView
@@ -51,6 +68,135 @@ class CameraFragment : Fragment() {
         cameraViewModel =
             ViewModelProvider(requireActivity()).get(CameraViewModel::class.java)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Intitialize Variables
+        navController = Navigation.findNavController(view)
+        doneBtn = view.findViewById(R.id.done_record_button)
+        recordBtn = view.findViewById(R.id.record_button)
+        timer = view.findViewById(R.id.record_timer)
+//        filenameText = view.findViewById<TextView>(R.id.record_filename)
+
+        /* Setting up on click listener
+           - Class must implement 'View.OnClickListener' and override 'onClick' method
+         */
+        binding.doneRecordButton.setOnClickListener{
+            if (isRecording) {
+                val alertDialog = AlertDialog.Builder(context)
+                alertDialog.setPositiveButton(
+                    "OKAY"
+                ) { dialog, which ->
+                    navController.navigate(R.id.photo_detail_view)
+                    isRecording = false
+                }
+                alertDialog.setNegativeButton("CANCEL", null)
+                alertDialog.setTitle("Audio Still recording")
+                alertDialog.setMessage("Are you sure, you want to stop the recording?")
+                alertDialog.create().show()
+            } else {
+                navController.navigate(R.id.photo_detail_view)
+            }
+        }
+        binding.recordButton.setOnClickListener{
+            if(isRecording){//double hit stop recording
+                stopRecording()
+                // Change button image and set Recording state to false
+                recordBtn.setImageDrawable(
+                    resources.getDrawable(
+                        R.drawable.ic_baseline_mic_24,
+                        null
+                    )
+                )
+                isRecording = false;
+            }else{
+                if(checkPermission()){
+                    startRecording()
+                    recordBtn.setImageDrawable(
+                        resources.getDrawable(
+                            R.drawable.ic_baseline_stop_24,
+                            null
+                        )
+                    )
+                    isRecording = false;
+                }
+
+            }
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        //TODO: ask for persmission
+        return true
+    }
+
+    private fun stopRecording() {
+
+        timer.stop()
+
+
+
+        //Stop media recorder and set it to null for further use to record new audio
+
+        //Stop media recorder and set it to null for further use to record new audio
+        mediaRecorder.stop()
+        mediaRecorder.release()
+        //mediaRecorder = null
+    }
+
+    private fun startRecording() {
+        //Start timer from 0
+
+        //Start timer from 0
+        timer.base = SystemClock.elapsedRealtime()
+        timer.start()
+
+        //Get app external directory path
+
+        //Get app external directory path
+        val recordPath = requireActivity()!!.getExternalFilesDir("/")!!.absolutePath
+
+        //Get current date and time
+
+        //Get current date and time
+        val formatter = SimpleDateFormat("yyyy_MM_dd_hh_mm_ss", Locale.CANADA)
+        val now = Date()
+
+        //initialize filename variable with date and time at the end to ensure the new file wont overwrite previous file
+
+        //initialize filename variable with date and time at the end to ensure the new file wont overwrite previous file
+        recordFile = "Recording_" + formatter.format(now) + ".3gp"
+
+
+        //Setup Media Recorder for recording
+
+        //Setup Media Recorder for recording
+
+        mediaRecorder = MediaRecorder(android.content.Context)
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        mediaRecorder.setOutputFile("$recordPath/$recordFile")
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+        try {
+            mediaRecorder.prepare()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        //Start Recording
+//        recorder = MediaRecorder(android.content.Context).apply {
+//            setAudioSource(MediaRecorder.AudioSource.MIC)
+//            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+//            setOutputFile(fileName)
+//            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)try {
+//            prepare()} catch (e: IOException) {Log.e(LOG_TAG, "prepare() failed")}
+//
+//            start()}
+        //Start Recording
+        mediaRecorder.start()
     }
 
     fun pickCamera() {
