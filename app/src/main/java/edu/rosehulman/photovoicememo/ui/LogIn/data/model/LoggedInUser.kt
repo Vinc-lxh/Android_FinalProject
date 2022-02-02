736e340455b4df5abecc0de221b110f73de3ef9d
Xingheng Lin <linx2@rose-hulman.edu>
2022-02-02 04:42:54 -0500

    remove unnecessary login fragment

--- app/src/main/java/edu/rosehulman/photovoicememo/ui/LogIn/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package edu.rosehulman.photovoicememo.ui.LogIn.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
