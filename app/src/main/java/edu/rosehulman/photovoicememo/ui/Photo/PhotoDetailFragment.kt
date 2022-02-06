17db4789b2e23698afcb70f264d0b151dc83c644
Xingheng Lin <linx2@rose-hulman.edu>
2022-02-05 23:47:27 -0500

    updated photo detail page

--- app/src/main/java/edu/rosehulman/photovoicememo/ui/Photo/PhotoDetailFragment.kt
+++ app/src/main/java/edu/rosehulman/photovoicememo/ui/Photo/PhotoDetailFragment.kt
@@ -1,6 +1,8 @@
 package edu.rosehulman.photovoicememo.ui.Photo
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,37 +12,71 @@ import coil.load
 import coil.transform.CircleCropTransformation
 import edu.rosehulman.photovoicememo.R
 import edu.rosehulman.photovoicememo.databinding.FragmentPhotoDetailBinding
+import edu.rosehulman.photovoicememo.model.Constants
+import edu.rosehulman.photovoicememo.model.PhotoVoice
+import edu.rosehulman.photovoicememo.model.PhotoVoiceViewModel
+import java.io.IOException
 
 class PhotoDetailFragment : Fragment() {
-   private  lateinit var model: PhotoViewModel
+   private var player: MediaPlayer? = null
+   private  lateinit var model: PhotoVoiceViewModel
    private lateinit var binding: FragmentPhotoDetailBinding
-
-
+   private var playing = true
+    private lateinit var photoVoice: PhotoVoice
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPhotoDetailBinding.inflate(inflater,container,false)
-
-        model = ViewModelProvider(requireActivity()).get(PhotoViewModel::class.java)
-
-//        updateView()
-        // Inflate the layout for this fragment
-//        binding.photoDetailView.load(R.drawable.avatar_11) {
-//            crossfade(true)
-//            transformations(CircleCropTransformation())
-//        }
+        model = ViewModelProvider(requireActivity()).get(PhotoVoiceViewModel::class.java)
+        photoVoice = model.getCurrentPhoto()
+        updateView()
+        setupButton()
         return binding.root
     }
 
-//    fun updateView(){
-//        binding.captionDetail.text =
-//
-//        binding.imageView.load(model.toString()) {
-//            crossfade(true)
-//            transformations(CircleCropTransformation())
-//        }
+    private fun setupButton() {
 
+        binding.playerPlayBtn.setOnClickListener {
+            if (playing) {
+                startPlaying()
+
+            } else {
+                stopPlaying()
+            }
+            playing = !playing
+
+        }
 
     }
+
+    fun updateView() {
+
+
+        binding.captionDetail.text = photoVoice.created.toString()
+        binding.photoDetailView.load(photoVoice.photo) {
+                crossfade(true)
+                transformations(CircleCropTransformation())
+        }
+    }
+
+
+
+    private fun startPlaying() {
+        player = MediaPlayer().apply {
+            try {
+                setDataSource(photoVoice.voice)
+                prepare()
+                start()
+            } catch (e: IOException) {
+                Log.e(Constants.TAG, "prepare() failed")
+            }
+        }
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+    }
+}
 
